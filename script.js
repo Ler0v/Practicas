ac41692c110282ac8c44bceb9c808fa7cba7219d
Agustin <90871968+Ler0v@users.noreply.github.com>
2022-07-21 16:30:46 -0300

    2

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function hacer (){
     for (i = 0; i < 3; i++){
         let nombre = prompt('Ingrese nombre')
         
-        if (nombre == nombre)
+        if (nasd)
 
         alert(`El 20% de su producto es: ${vPorciento} 
             El 50% de su producto es: ${cPorciento}
