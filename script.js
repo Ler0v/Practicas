eb3b24ee674fff923773867b4cf5c4b6901f9598
Agustin <90871968+Ler0v@users.noreply.github.com>
2022-07-21 17:26:49 -0300

    8

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 alert('Bienvenido')
 
 
-function hacer (){
-    
-    for (i = 0; i < 5; i++){
-        let letraIngresada = prompt('ingrese una letra')
+function hacer () {
 
-        if (letraIngresada == "y" || letraIngresada == "Y"){
-            alert('Es correcto')
+    let numero = parseInt(prompt('Selecciona un numero del 1 al 4'))
+
+    switch (numero){
+        case 1:
+            alert('Su personaje es Homero')
+            break;
+        case 2:
+            alert('Su personaje es Marge')
+            break;
+        case 3:
+            alert('Su personaje es Bart')
+            break;
+        case 4:
+            alert('Su personaje es Lisa')
+            break;
+        default:
+            alert('Anda a decidir entre los 4 pibe')
         }
-        else{
-            alert('ingresar la letra correcta')
-        }
-    }
+
 }
