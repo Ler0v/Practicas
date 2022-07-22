2c2e383076df72c7c97ba9d228a6feed4ec932f3
Agustin <90871968+Ler0v@users.noreply.github.com>
2022-07-22 09:51:03 -0300

    9

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
-alert('Bienvenido')
-
 
 function hacer () {
 
-    let numero = parseInt(prompt('Selecciona un numero del 1 al 4'))
+    let numero = prompt('Ingrese su presupuesto')
 
-    switch (numero){
-        case 1:
-            alert('Su personaje es Homero')
+    while (numero != "esc"){
+        if (numero > 0 && numero <= 1000){
+            alert("Su presupuesto es bajo")
             break;
-        case 2:
-            alert('Su personaje es Marge')
-            break;
-        case 3:
-            alert('Su personaje es Bart')
-            break;
-        case 4:
-            alert('Su personaje es Lisa')
-            break;
-        default:
-            alert('Anda a decidir entre los 4 pibe')
         }
+        else if(numero > 1001 && numero <= 3000){
+            alert('Su presupuesto es intermedio')
+            break;
+        }
+        else if (numero > 5000){
+            alert('Su presupuesto es alto')
+            break;
+        }
+    }
 
 }
