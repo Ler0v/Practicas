9cc97acc3ce3f635201057536f6230d8965f3051
Agustin <90871968+Ler0v@users.noreply.github.com>
2022-07-22 14:37:38 -0300

    9

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 
 function hacer () {
 
-    let numero = prompt('Ingrese su presupuesto')
+    let productos = ["leche","pan","queso","yerba"]
 
-    while (numero != "esc"){
-        if (numero > 0 && numero <= 1000){
-            alert("Su presupuesto es bajo")
-            break;
-        }
-        else if(numero > 1001 && numero <= 3000){
-            alert('Su presupuesto es intermedio')
-            break;
-        }
-        else if (numero > 5000){
-            alert('Su presupuesto es alto')
-            break;
-        }
+    let entrada = prompt('Ingrese el producto')
+    
+    let noEstaCargado = true
+
+    productos.forEach(element => {
+    
+            if (entrada == element){
+                alert('El producto esta cargado.')
+                noEstaCargado = false
+            }
+            
+    });
+
+    if (noEstaCargado == true){
+        alert('El producto no esta cargado.')
     }
 
 }
