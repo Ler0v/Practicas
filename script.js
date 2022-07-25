e17b7f38b6b508e84f4409eaca9ff953a26cd126
Agustin <90871968+Ler0v@users.noreply.github.com>
2022-07-25 17:53:59 -0300

    16 (resumidisimo)

--- script.js
+++ script.js
@@ -1,22 +1,7 @@
-function hacer () {
+function hacer (){
 
-    numero = prompt('Ingresar numero')
+    let valor = parseInt(prompt('ingrese un valor'))
 
-    while(numero != "ESC"){
-        if (numero == 1){
-            alert('Seleccionaste tomate')
-            break;
-        }else if (numero == 2){
-            alert('Seleccionaste papa')
-            break;
-        }else if (numero == 3){
-            alert('Seleccionaste carne')
-            break;
-        }else if (numero == 4){
-            alert('Seleccionaste pollo')
-            break;
-        }
-
-    }
+    alert(valor + 10)
 
 }
